e0cdddc6a064571fb6a1057807e9077ae0b1b114
jordanrath <rath.jordan@icloud.com>
2022-11-01 14:37:12 -0600

    added project pages

--- src/data/ModalData.js
+++ src/data/ModalData.js
@@ -28,7 +28,7 @@ export const ModalData = [
                 name: 'Food Tracker',
                 internalName: 'foodLog',
                 description: 'Food Tracker is built with JavaScript, HTML and CSS.  With Food Tracker you can select from a number of foods and customize the macro nutrients that food has.  Once a food has been added Food Tracker will calculate the total calories and add the results to a graph that stores data of all foods that have been added.',
-                href: "https://jordanrath.github.io/supermarket/",
+                href: "https://jordanrath.github.io/nutrition-log/?",
                 images: [ 
                  {
                      image: '/modalImages/foodlogFrontPage.png',
@@ -49,7 +49,7 @@ export const ModalData = [
                 id: 3,
                 name: 'Online Supermarket',
                 internalName: 'superMarket',
-                description: 'Supermarket is built with React, HTML and CSS.  Easily navigate through the store and add foods to your cart.  You can edit your cart when ready, you will be sent to checkout with Stripe integration.',
+                description: 'Supermarket is built with React and CSS.  Easily navigate through the store and add foods to your cart.  You can edit your cart when ready, you will be sent to checkout with Stripe integration.',
                 href: "https://jordanrath.github.io/supermarket/",
                 images: [                
                  {
@@ -67,6 +67,32 @@ export const ModalData = [
                  {
                      image: '/modalImages/superMStripe.png',
                      alt: 'React Supermarket stripe checkout',
+                 },
+                ]
+            },
+            {
+                id: 4,
+                name: 'Weather App',
+                internalName: 'weatherApp',
+                description: 'Weather App is built with React and CSS, it features backend hookups to provide geolocation updated by zip code.',
+                href: "https://jordanrath.github.io/weather-app/",
+                images: [                
+                 {
+                     image: '/modalImages/weatherApp.png',
+                     alt: 'Weather App main page.',
+                 },
+                ]
+            },
+            {
+                id: 5,
+                name: 'Pomodoro Clock',
+                internalName: 'pomodoroClock',
+                description: 'Pomodoro clock built with React and CSS.  Set a countdown clock and take a break once you hear the bell!',
+                href: "https://jordanrath.github.io/clock-app/",
+                images: [                
+                 {
+                     image: '/modalImages/pomodoroClock.png',
+                     alt: 'Pomodoro Clock main page.',
                  },
                 ]
             },
