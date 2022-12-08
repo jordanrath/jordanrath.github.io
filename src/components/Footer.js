578582e9bf408e3e758125f14cabd5a08270486c
jordanrath <rath.jordan@icloud.com>
2022-12-08 13:06:17 -0700

    fixed github footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className='footer footer-div' id='footer'>
       <div className='contact-info-div' data-aos="flip-left">
-        <a id='profile-link' className='contact-info' href="https://github.com/jordanrath?tab=projects" target="_blank" rel="noreferrer"><FontAwesomeIcon icon="fa-brands fa-github-square" />GitHub</a>
+        <a id='profile-link' className='contact-info' href="https://github.com/jordanrath" target="_blank" rel="noreferrer"><FontAwesomeIcon icon="fa-brands fa-github-square" />GitHub</a>
         <a id='profile-link' className='contact-info' href="https://www.linkedin.com/in/jordan-rath-75234a249/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon="fa-brands fa-linkedin-in" />LinkedIn</a>
         <a id='profile-link' className='contact-info' href="mailto:rath.jordan@icloud.com" target="_blank" rel="noreferrer"><FontAwesomeIcon icon="fa-regular fa-envelope" />Email</a>
       </div>
