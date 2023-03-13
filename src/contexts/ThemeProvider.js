18f7bfc03231e1caef9af70994ad227a54bcd68c
jordanrath <rath.jordan@icloud.com>
2023-03-13 14:05:20 -0600

    created theme context provider

---
+++ src/contexts/ThemeProvider.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { ThemeContext, themes } from './ThemeContext'
+
+const ThemeProvider = (props) => {
+    const [theme, setTheme] = useState(themes.dark);
+
+    const changeTheme = (theme) => {
+        setTheme(theme);
+    }
+
+    useEffect(() => {
+      switch (theme) {
+        case themes.light:
+            document.body.classList.add('light-mode');
+            break;
+        case themes.dark:
+        default:
+            document.body.classList.remove('light-mode');
+            break;
+      }
+    }, [theme])
+    
+
+  return (
+    <ThemeContext.Provider value={{ theme: theme, changeTheme: changeTheme }}>
+        {props.children}
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeProvider;
