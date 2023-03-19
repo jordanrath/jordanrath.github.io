fd237b30e03913a80444fa5e32dcec7aea9a13ef
jordanrath <rath.jordan@icloud.com>
2023-03-19 14:55:13 -0600

    updated certification descriptions

--- src/data/EducationData.js
+++ src/data/EducationData.js
@@ -8,7 +8,7 @@ export const EducationData = [
             alt: 'Learn React Certification',
             subtitle: 'Learn React',
             title: 'React Certification',
-            description: 'The learn React certification dives into the details of React covering everthing from React.createElement to using and creating advanced hooks.'
+            description: 'Dives into the details of React covering everthing from React.createElement to using and creating advanced hooks.'
       },
       {
             imageShown: '/images/JsCert.png',
@@ -16,7 +16,7 @@ export const EducationData = [
             alt: 'Learn JavaScript Certification',
             subtitle: 'Learn JavaScript',
             title: 'JavaScript Certification',
-            description: 'The learn JavaScript certification went in depth into JavaScript covering basic to advanced topics.'
+            description: 'In depth introduction to JavaScript covering basic topics and syntaxes to advanced topics such as async/await and recursion.'
       },
       {
             imageShown: '/images/JsCertFCC.png',
@@ -24,7 +24,7 @@ export const EducationData = [
             alt: 'Free Code Camp JavaScript Certification',
             subtitle: 'Free Code Camp',
             title: 'JavaScript Certification',
-            description: 'The JavaScript certification covers JavaScript fundamentals with usage of Object Oriented Programming and Functional Programming.'
+            description: 'Covers JavaScript fundamentals with an emphasis on Object Oriented Programming and Functional Programming.'
       },
       {
             imageShown: '/images/FrontendCertFCC.png',
@@ -32,7 +32,7 @@ export const EducationData = [
             alt: 'Free Code Camp Front End Certification',
             subtitle: 'Free Code Camp',
             title: 'Front End Certification',
-            description: 'The front end certification utilized Bootstrap to style the projects and extended the CSS logic with Sass.  The course also focused on building single page applications going in depth using React and Redux.'
+            description: 'Utilized popular frameworks to build single page apps such as CSS, Bootstrap, Sass, React and Redux.'
       },
       {
             imageShown: '/images/WebDesignCertFCC.png',
@@ -40,7 +40,7 @@ export const EducationData = [
             alt: 'Free Code Camp Web Design Certification',
             subtitle: 'Free Code Camp',
             title: 'Web Design Certification',
-            description: 'The responsive web design certification covers the building of responsive website with HTML and CSS.  It dives into the usage of flexbox, grid, and much more to build responsive web applications.'
+            description: 'Teaches principles of responsive website design such as flexbox, grid, and media queries.'
       },
       {
             imageShown: '/images/BackEndCertFCC.png',
@@ -48,6 +48,6 @@ export const EducationData = [
             alt: 'Free Code Camp Back End and API Certification',
             subtitle: 'Free Code Camp',
             title: 'Back End and API Certification',
-            description: 'The back end and api certification focuses on writing back end apps with Node.js and npm.  There is also attention paid to building with the Express framework and hooking it all together with MongoDB and the Mongoose library.'
+            description: 'Built several backend applications utilizing libraries such as Node.js, Express, Mongoose, and MongoDB.'
       },
 ];
